808922f064f13b7e4d7a62732d056f8f790bf1a2
Lee, Seungjin <quack337@gmail.com>
2022-12-20 14:19:41 +0900

    Main.java 수정2

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
+
+    static void say(String s) {
+        System.out.println(s);
+    }
     
     public static void main(String[] args) {
-        String s = "hello world";
-        System.out.println(s);
+        say("hello world");
     }
 }
 
